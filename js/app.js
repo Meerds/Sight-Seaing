79d22e2fb16e996e5044eb30357001bf0a104338
Meerds <diogofreitascastro@gmail.com>
2019-12-05 10:59:03 +0000

    + header JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation()
+jQuery(document).ready(function($) {
+    // Scroll effects
+    $(window).scroll(function () {
+
+        var height = $(window).scrollTop();
+
+        if(height  > 50) {
+            $( "#common-page-header" ).addClass("fixed-header");
+            $("#tiles-wrapper").addClass("fixed-tiles");
+        }
+
+        else if(height  < 1) {
+            $( "#common-page-header" ).removeClass("fixed-header");
+            $("#tiles-wrapper").removeClass("fixed-tiles");
+        }
+    });
+});
